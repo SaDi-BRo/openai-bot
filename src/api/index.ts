bd25433b7ca9d3793241c6bb5f5729c9e3eda866
SaDi <sayidullohabbasov0909@gmail.com>
2023-02-28 21:28:11 +0500

    added middleware

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,4 +2,8 @@ import { Bot, webhookCallback } from 'grammy';
 
 const bot = new Bot(process.env.BOT_TOKEN!);
 
+bot.on('message', async ctx => {
+  await ctx.reply('Hi there!');
+});
+
 export default webhookCallback(bot, 'http');
