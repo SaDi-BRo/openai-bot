6c9c8898338c5ad5893537e9603b24d853522f7b
SaDi <sayidullohabbasov0909@gmail.com>
2023-03-02 22:53:52 +0500

    deleted all files

--- src/actions/text.ts
+++
@@ -1,8 +0,0 @@
-import { bot } from '../core/bot';
-import { fetchOpenAIResponse } from '../core/openai';
-
-bot.on('msg:text', async ctx => {
-  const res = await fetchOpenAIResponse(ctx.message!.text);
-  const message = res ?? 'Something went wrong';
-  await ctx.reply(message);
-});
--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Bot, webhookCallback } from 'grammy';
-
-const bot = new Bot(process.env.BOT_TOKEN!);
-
-bot.on('message', async ctx => {
-  await ctx.reply('Hi there!');
-});
-
-export default webhookCallback(bot, 'http');
